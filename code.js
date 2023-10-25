013b59b44624ea8f4dda831df17766caaee51f35
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-10-24 23:20:42 -0600

    adj matrix successfully turned into an adj list, test has not been updated yet, also added run time analysis

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    var adjList = [];
+    var tempList
+
+    if (adjMatrix == []){
+        return []
+    }
+
+    for (i = 0; i < adjMatrix.length; i++){
+        tempList = [];
+        for(j = 0; j < adjMatrix[i].length; j++){
+            if(adjMatrix[i][j] == 1) {
+                tempList.push(j + 1);
+            }
+        }
+        adjList.push(tempList);
+    }
+
+    return adjList;
 }
